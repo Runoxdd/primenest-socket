3f7b785b0de4e536183948459c775c440023860c
Runoxdd <runoefekemo@gmail.com>
2026-02-06 14:20:15 +0100

    socket init

---
+++ app.js
@@ -0,0 +1,45 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    // Allows your local machine and your future hosted site
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+  },
+});
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
+
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+
+// VERY IMPORTANT: Render uses process.env.PORT
+io.listen(process.env.PORT || 4000);
